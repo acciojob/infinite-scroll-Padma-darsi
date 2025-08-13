7f4e679610f69bd1b75ce79a3f2b834c7573ab73
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-08-13 18:28:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+let itemCount = 10; // already have 10 items
+
+// Function to add items
+function addItems(count) {
+  for (let i = 0; i < count; i++) {
+    itemCount++;
+    const li = document.createElement("li");
+    li.textContent = `item ${itemCount}`;
+    list.appendChild(li);
+  }
+}
+
+// Scroll event
+window.addEventListener("scroll", () => {
+  // Check if we reached the bottom
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    addItems(2); // Add 2 more items
+  }
+});
